bc52ba26f9334a5878db00f4172eebf0c9501d44
ErenKarakaya <erenkarakaya@eskisehir.edu.tr>
2023-10-10 15:25:40 +0300

    admin added

--- src/utils/isAdmin.ts
+++ src/utils/isAdmin.ts
@@ -2,6 +2,7 @@ const isAdmin = (uid: string) => {
   const adminList = [
     "Xh7z8uSKFgOrEIpBLfTAuwqS49H2",
     "2GLIPPawLqO3t8WIb74Z2MpBic03",
+    "qpG3Sl2k3sXsspLVATM9uxwDTnG2"
   ]
 
   return adminList.includes(uid)
